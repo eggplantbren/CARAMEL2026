bcabe115986908a619df370a34f4809fd7bf0bb4
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-12-22 10:39:38 +1300

    Some constants

---
+++ Constants.h
@@ -0,0 +1,14 @@
+#ifndef CARAMEL2026_Constants_h
+#define CARAMEL2026_Constants_h
+
+namespace CARAMEL2026
+{
+
+// Continuum-related constants
+constexpr int n_continuum_interp = 1000;
+constexpr double continuum_back_extrap = 0.5;
+
+} // namespace
+
+#endif
+
